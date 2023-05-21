ad5cb712c5da64d52bc5246f218df4cbbb06a41f
root <root@ip-172-31-9-61.ap-northeast-3.compute.internal>
2023-05-21 15:49:06 +0000

    1. first working bot with discord.py

--- work-assistant/bot.py
+++ work-assistant/bot.py
@@ -1,20 +1,16 @@
 import os
-from interactions import Client, Intents, listen
-
-bot = Client(intents=Intents.DEFAULT)
-# intents are what events we want to receive from discord, `DEFAULT` is usually fine
-
-@listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
-async def on_ready():
-    # This event is called when the bot is ready to respond to commands
-    print("Ready")
-    print(f"This bot is owned by {bot.owner}")
 
 
-@listen()
-async def on_message_create(event):
-    # This event is called when a message is sent in a channel the bot can see
-    print(f"message received: {event.message.content}")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 bot_token = os.environ.get("DISCORD_BOT_TOKEN")
-bot.start(bot_token)
+bot.run(bot_token)
